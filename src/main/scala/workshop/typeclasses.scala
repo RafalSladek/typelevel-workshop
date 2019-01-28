18331de14d57e089abc254820a590716d5d32818
Rafal Sladek <rafal.sladek@gmail.com>
2019-01-28 11:52:06 +0100

    monoid

--- src/main/scala/workshop/typeclasses.scala
+++ src/main/scala/workshop/typeclasses.scala
@@ -26,12 +26,17 @@ object typeclasses {
     def show(a: String): String = a
   }
 
-  implicit def showChessPiece: Show[ChessPiece] = ???
+  implicit def showChessPiece: Show[ChessPiece] = new Show[ChessPiece] {
+    def show(a: ChessPiece): String =
+      s"""${a.chessType.getClass.getCanonicalName} : ${a.position.x.toString}, ${a.position.y.toString}"""
+  }
 
-  implicit def showOption[A: Show]: Show[Option[A]] = ???
-
-
-
+  implicit def showOption[A: Show]: Show[Option[A]] = new Show[Option[A]] {
+    def show(a: Option[A]): String = a match {
+      case None       => "None"
+      case Some(x: A) => "Some(x)"
+    }
+  }
 
   //Eq
 
@@ -48,13 +53,21 @@ object typeclasses {
     def eqv(x: String, y: String): Boolean = x.equals(y)
   }
 
-  implicit def eqOption[A: Eq]: Eq[Option[A]] = ???
+  implicit def eqOption[A: Eq]: Eq[Option[A]] = new Eq[Option[A]] {
+    def eqv(x: Option[A], y: Option[A]): Boolean = (x, y) match {
+      case (Some(a), Some(b)) => a === b
+      case (None, None)       => true
+      case _                  => false
+    }
+  }
 
-  implicit def eqEither[A: Eq, B: Eq]: Eq[Either[A, B]] = ???
-
-
-
-
+  implicit def eqEither[A: Eq, B: Eq]: Eq[Either[A, B]] = new Eq[Either[A, B]] {
+    def eqv(x: Either[A, B], y: Either[A, B]): Boolean = (x, y) match {
+      case (Left(a), Left(b))   => a === b
+      case (Right(a), Right(b)) => a === b
+      case _                    => false
+    }
+  }
 
   //Monoid
 
@@ -71,11 +84,24 @@ object typeclasses {
     def combine(x: Int, y: Int): Int = x + y
   }
 
-  implicit def stringMonoid: Monoid[String] = ???
+  implicit def stringMonoid: Monoid[String] = new Monoid[String] {
+    def empty: String = ""
 
-  implicit def timespanMonoid: Monoid[TimeSpan] = ???
+    def combine(x: String, y: String): String = x + y
+  }
 
-  implicit def listMonoid[A]: Monoid[List[A]] = ???
+  implicit def timespanMonoid: Monoid[TimeSpan] = new Monoid[TimeSpan] {
+    def empty: TimeSpan = TimeSpan(0, Second)
+
+    def combine(x: TimeSpan, y: TimeSpan): TimeSpan = addTimeSpan(x, y)
+  }
+
+  implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+    def empty: List[A] = List.empty
+
+    def combine(x: List[A], y: List[A]): List[A] = x ::: y
+
+  }
 
   // The intMonoid further up uses `addition` as its method of combination, but that is not the only monoid for `Int`!
   // We can also define one for multiplication, though if we just define it for `Int` again the compiler won't know which to pick
@@ -90,12 +116,12 @@ object typeclasses {
 
   implicit def tupleMonoid[A: Monoid, B: Monoid]: Monoid[(A, B)] = ???
 
-  implicit def tuple3Monoid[A: Monoid, B: Monoid, C: Monoid]: Monoid[(A, B, C)] = ???
+  implicit def tuple3Monoid[A: Monoid, B: Monoid, C: Monoid]
+    : Monoid[(A, B, C)] = ???
 
   implicit def mapMonoid[A, B: Monoid]: Monoid[Map[A, B]] = ???
 
   implicit def futureMonoid[A: Monoid]: Monoid[Future[A]] = ???
-
 
   //Monoid word count
   //Use foldMap with a Monoid to count the number of words, the number of characters and the number of occurences of each word
@@ -103,19 +129,8 @@ object typeclasses {
 
   val words: List[String] = rawText.split(" ").toList
 
-
-
-
-
-
   //Now that you have the word count let's extend it with the ability to get the longest word of the text.
   //Tip: Define a Maximum Monoid to do so
-
-
-
-
-
-
 
   //Functor
 
@@ -127,18 +142,11 @@ object typeclasses {
 
   implicit def listFunctor: Functor[List] = ???
 
-
-
   sealed trait Tree[+A]
   case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
   case class Leaf[A](value: A) extends Tree[A]
 
   implicit def treeFunctor: Functor[Tree] = ???
-
-
-
-
-
 
   //Cardinality
 
@@ -156,16 +164,17 @@ object typeclasses {
 
   implicit def cardinalityInt: Cardinality[Int] = ???
 
-  implicit def cardinalityTuple[A: Cardinality, B: Cardinality]: Cardinality[(A, B)] = ???
+  implicit def cardinalityTuple[A: Cardinality, B: Cardinality]
+    : Cardinality[(A, B)] = ???
 
-  implicit def cardinalityEither[A: Cardinality, B: Cardinality]: Cardinality[Either[A, B]] = ???
+  implicit def cardinalityEither[A: Cardinality, B: Cardinality]
+    : Cardinality[Either[A, B]] = ???
 
   implicit def cardinalitySize: Cardinality[Size] = ???
 
-  implicit def cardinalityNothing: Cardinality[Nothing]= ???
+  implicit def cardinalityNothing: Cardinality[Nothing] = ???
 
-  implicit def cardinalityFunction[A: Cardinality, B: Cardinality]: Cardinality[A => B] = ???
-
-
+  implicit def cardinalityFunction[A: Cardinality, B: Cardinality]
+    : Cardinality[A => B] = ???
 
 }
