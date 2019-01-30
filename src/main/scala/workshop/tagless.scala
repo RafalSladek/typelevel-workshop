d181c6399e9a77e580aa99a704ebb8699fbe16dc
Rafal Sladek <rafal.sladek@gmail.com>
2019-01-30 13:55:16 +0100

    add fileProgram with console[IO]

--- src/main/scala/workshop/tagless.scala
+++ src/main/scala/workshop/tagless.scala
@@ -1,8 +1,11 @@
 package workshop
 
+import java.io.{File, FileNotFoundException}
+
 import workshop.monoids.{IO, Monad}
 import workshop.monoids.Monad.ops._
-import scala.io.StdIn
+
+import scala.io.{Source, StdIn}
 
 object tagless {
 
@@ -49,15 +52,47 @@ object tagless {
     def readLine: Id[String] = "Rafal"
   }
 
-  def testconsoleProgramId = consoleProgram[Id]
+  def testconsoleProgramId: Id[Unit] = consoleProgram[Id]
 
   // Create a Tagless algebra for accesing the file system
-  trait FileSystem[F[_]]
+  trait FileSystem[F[_]] {
+    def readFile(file: File): F[Either[String, String]]
+  }
 
   object FileSystem {
     def apply[F[_]](implicit ev: FileSystem[F]): FileSystem[F] = ev
   }
 
+  implicit def fileSystemIO: FileSystem[IO] = new FileSystem[IO] {
+
+    def readFile(file: File): IO[Either[String, String]] =
+      IO(() =>
+        try { Right(Source.fromFile(file).getLines().mkString) } catch {
+          case t: FileNotFoundException => Left("File not found")
+      })
+  }
+
   // Rewrite the `monoids.fileProgram` from earlier using tagless final style
-  def fileProgram = ???
+  def fileProgram[F[_]: Monad: Console: FileSystem]: F[Unit] =
+    for {
+      _ <- Console[F].printLine("Please provide file name")
+      fileName <- Console[F].readLine
+      fileOrError <- FileSystem[F].readFile(new File(fileName))
+      _ <- Console[F].printLine(fileOrError.merge)
+
+    } yield ()
+
+  // fileProgram[F[_]: Monad: Console: FileSystem] is equivalent with fileProgram[F[_]](implicit m: Monad[F], c: Console[F],f: FileSystem[F])
+  def fileProgram2[F[_]](implicit m: Monad[F],
+                         c: Console[F],
+                         f: FileSystem[F]): F[Unit] =
+    for {
+      _ <- Console[F].printLine("Please provide file name")
+      fileName <- Console[F].readLine
+      fileOrError <- FileSystem[F].readFile(new File(fileName))
+      _ <- Console[F].printLine(fileOrError.merge)
+
+    } yield ()
+
+  def testfileProgramConsole = fileProgram[IO].unsafeRun()
 }
